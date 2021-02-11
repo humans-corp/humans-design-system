65721deb42c8935eb57d389eb9b7045a03560bc8
Shine Alexander <>
2021-02-12 00:02:29 +0900

    [MIG-357] button component 중간 완성

--- ui-components/src/App.tsx
+++ ui-components/src/App.tsx
@@ -1,20 +1,193 @@
 import React from "react";
-import ButtonComponent, { Size, Color, Type } from "./Button/ButtonComponent";
+import ButtonComponent, { ButtonSize, ButtonColor } from "./Button/ButtonComponent";
 
 function Button() {
   return (
-    <div className="App">
-      <ButtonComponent
-        type={Type.BUTTON}
-        height={Size.MEDIUM}
-        fontSize={Size.SMALL}
-        borderRadius={4}
-        background={Color.PRIMARY}
-        isDisabled={true}
-      >
-        TESTBUTTON
-      </ButtonComponent>
-    </div>
+    <>
+      {/* NOTE prefix 는 vscode COMMENT ANCHOR extension 입니다. */}
+      <h1>BLOCK BUTTON</h1>
+      <div className="App">
+        {/* NOTE PRIMARY LARGE BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.LARGE}
+          isBlock={true}
+          colors={ButtonColor.PRIMARY}
+        >
+          PRIMARY LARGE BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC LARGE BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.LARGE}
+          isBlock={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC LARGE BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC LARGE BLOCK DISABLED BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.LARGE}
+          isBlock={true}
+          disabled={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC LARGE BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE PRIMARY MEDIUM BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.MEDIUM}
+          isBlock={true}
+          colors={ButtonColor.PRIMARY}
+        >
+          PRIMARY MEDIUM BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC MEDIUM BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.MEDIUM}
+          isBlock={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC MEDIUM BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC MEDIUM BLOCK DISABLED BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.MEDIUM}
+          isBlock={true}
+          disabled={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC MEDIUM BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE PRIMARY SMALL BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.SMALL}
+          isBlock={true}
+          colors={ButtonColor.PRIMARY}
+        >
+          PRIMARY SMALL BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC SMALL BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.SMALL}
+          isBlock={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC SMALL BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC SMALL BLOCK DISABLED BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.SMALL}
+          isBlock={true}
+          disabled={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC SMALL BLOCK BUTTON
+        </ButtonComponent>
+      </div>
+
+      <h1>NON-BLOCK BUTTON</h1>
+      <div className="App">
+        {/* NOTE PRIMARY LARGE NON-BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.LARGE}
+          colors={ButtonColor.PRIMARY}
+        >
+          PRIMARY LARGE NON-BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC LARGE NON-BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.LARGE}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC LARGE NON-BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC LARGE NON-BLOCK DISABLED BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.LARGE}
+          disabled={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC LARGE NON-BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE PRIMARY MEDIUM NON-BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.MEDIUM}
+          colors={ButtonColor.PRIMARY}
+        >
+          PRIMARY MEDIUM NON-BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC MEDIUM NON-BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.MEDIUM}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC MEDIUM NON-BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC MEDIUM NON-BLOCK DISABLED BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.MEDIUM}
+          disabled={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC MEDIUM NON-BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE PRIMARY SMALL NON-BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.SMALL}
+          colors={ButtonColor.PRIMARY}
+        >
+          PRIMARY SMALL NON-BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC SMALL NON-BLOCK BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.SMALL}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC SMALL NON-BLOCK BUTTON
+        </ButtonComponent>
+
+        {/* NOTE BASIC SMALL NON-BLOCK DISABLED BUTTON */}
+        <ButtonComponent
+          type='button'
+          size={ButtonSize.SMALL}
+          disabled={true}
+          colors={ButtonColor.BASIC}
+        >
+          BASIC SMALL NON-BLOCK BUTTON
+        </ButtonComponent>
+      </div>
+    </>
   );
 }
 
--- ui-components/src/Button/ButtonComponent.tsx
+++ ui-components/src/Button/ButtonComponent.tsx
@@ -3,98 +3,91 @@ import styled from '@emotion/styled';
 import { PropsWithChildren } from 'react';
 import HdsColor from '../HdsColor';
 
-export enum Type {
-  BUTTON = 'button',
-  SUBMIT = 'submit',
-  RESET = 'reset',
-}
-
-export enum Size {
+export enum ButtonSize {
   LARGE = 'large',
   MEDIUM = 'medium',
   SMALL = 'small',
 }
 
-export enum Color {
+export enum ButtonColor {
   BASIC = 'basic',
   PRIMARY = 'primary',
 }
 
 type Props = {
-  type?: Type;
+  type?: 'button' | 'submit' | 'reset';
   isBlock?: boolean;
-  height?: Size;
+  size?: ButtonSize;
   borderColor?: string;
-  borderRadius?: number;
-  fontSize?: Size;
-  color?: Color;
-  background?: Color;
-  isDisabled?: boolean;
+  isRounded?: boolean;
+  colors?: ButtonColor;
+  disabled?: boolean;
   onClick?(): void;
 };
 
-const ComponentHeight = (size: Size = Size.SMALL): string => ({
+const ComponentSize = (size: ButtonSize = ButtonSize.SMALL): string => ({
   small: '44',
   medium: '68',
   large: '83',
 }[size]);
 
-const ComponentFontSize = (size: Size = Size.SMALL): string => ({
-  large: '24',
-  medium: '24',
+const ComponentTextSize = (size: ButtonSize = ButtonSize.SMALL): string => ({
   small: '16',
+  medium: '24',
+  large: '24',
 }[size]);
 
-const ComponentBackground = (color: Color = Color.BASIC): string => ({
-  basic: HdsColor.HDSGRAY100,
-  primary: HdsColor.HDSBLUE500,
+const ComponentBackground = (color: ButtonColor = ButtonColor.BASIC, disabled: boolean = false): string => ({
+  basic: disabled ? HdsColor.HDSGRAY400 : HdsColor.HDSGRAY100,
+  primary: disabled ? HdsColor.HDSGRAY400 : HdsColor.HDSBLUE500,
 }[color]);
 
-const ComponentColor = (color: Color = Color.BASIC): string => ({
-  basic: '#fff',
-  primary: '#fff',
+const ComponentTextColor = (color: ButtonColor = ButtonColor.BASIC, disabled: boolean = false): string => ({
+  basic: disabled ? HdsColor.HDSWHITE : HdsColor.HDSGRAY900,
+  primary: disabled ? HdsColor.HDSWHITE : HdsColor.HDSWHITE,
 }[color]);
 
 const ButtonComponent = styled.button<Props>`
   appearance: none;
   display: ${props => props.isBlock ? 'flex' : 'inline-flex'};
+  width: ${props => props.isBlock ? '100%' : 'auto'};
+  min-width: 68px;
   align-items: center;
   justify-content: center;
-  height: ${props => `${ComponentHeight(props.height)}px`};
+  height: ${props => `${ComponentSize(props.size)}px`};
   ${props => props.borderColor
     ? `border: 1px solid ${props.borderColor}`
     : `border: 0`};
-  ${props => props.borderRadius && `border-radius: ${props.borderRadius}px;`};
+  ${props => props.isRounded && `border-radius: 4px;`};
+  padding: 5px 15px;
   box-sizing: border-box;
-  background: ${props => props.background ? `${ComponentBackground(props.background)}` : `${ComponentBackground()}`};
-  font-size: ${props => props.fontSize ? `${ComponentFontSize(props.fontSize)}px` : `${ComponentFontSize()}px`};
-  color: ${props => props.color ? `${ComponentColor(props.color)}` : `${ComponentColor()}`}
+  background: ${props => `${ComponentBackground(props.colors, props.disabled)}`};
+  font-size: ${props => `${ComponentTextSize(props.size)}px`};
+  color: ${props => props.colors ? `${ComponentTextColor(props.colors, props.disabled)}` : `${ComponentTextColor()}`};
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'}
 `;
 
 const Button = ({
   type,
   isBlock,
-  height,
+  size,
   borderColor,
-  borderRadius,
-  fontSize,
-  color,
-  background,
-  isDisabled,
-  
+  isRounded,
+  colors,
+  disabled,
   children,
   onClick,
 }: PropsWithChildren<Props>) => (
+  // NOTE disabled 를 isDisabled 로 사용하지 않는 이유는
+  // isDisabled 로 쓰면 button 의 attribute 인 disabled 할당이 안되기 때문
   <ButtonComponent
     type={type}
     isBlock={isBlock}
-    height={height}
+    size={size}
     borderColor={borderColor}
-    borderRadius={borderRadius}
-    fontSize={fontSize}
-    color={color}
-    background={background}
-    isDisabled={isDisabled}
+    isRounded={isRounded}
+    colors={colors}
+    disabled={disabled}
     onClick={onClick}
   >
     {children}
